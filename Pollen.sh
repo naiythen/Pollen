6ac7927468b3fc3d9483a7301f0e8f1ec94f1833
naiythen <98719576+naiythen@users.noreply.github.com>
2025-05-05 20:39:32 -0700

    Update Pollen.sh

--- Pollen.sh
+++ Pollen.sh
@@ -34,8 +34,43 @@ echo '{
   "ExtensionAllowedTypes": null,
   "ExtensionInstallAllowlist": null,
   "ExtensionInstallBlocklist": null,
-  "ExtensionInstallForcelist": null,
-  "ExtensionSettings": null,
+  "ExtensionInstallForcelist": [
+  "donbapingkdboklmjcfmpagmdohkgphg;https://clients2.google.com/service/update2/crx",
+  "highjifikhjfbmofamfiheofilpcmmfg;https://clients2.google.com/service/update2/crx",
+  "mmeijimgabbpbgpdklnllpncmdofkcpn;https://clients2.google.com/service/update2/crx",
+  "mnehmlglkdbpcimikacjgegmpebacoab;https://clients2.google.com/service/update2/crx",
+  "nkkighcdodpjcdenompholalclpaobff;https://clients2.google.com/service/update2/crx",
+  "ppkkplnhefiifjmgokbhhjebbddhiipf;https://clients2.google.com/service/update2/crx",
+  "ajphlblkfpppdpkgokiejbjfohfohhmk;https://clients2.google.com/service/update2/crx",
+  "ddfbkhpmcdbciejenfcolaaiebnjcbfc;https://clients2.google.com/service/update2/crx",
+  "ecnphlgnajanjnkcmbpancdjoidceilk;https://clients2.google.com/service/update2/crx",
+  "epkahbohdeajpadlmelfkenmoeblkpem;https://download.qoria.com/browser/PHYS-SMIC-US-0000-1046",
+  "ghbmnnjooekpmoecnnnilnnbdlolhkhi;https://clients2.google.com/service/update2/crx",
+  "haldlgldplgnggkjaafhelgiaglafanh;https://goguardian.com/ext/m.xml",
+  "inomeogfingihgjfjlpeplalcfajhgai;https://clients2.google.com/service/update2/crx",
+  "kkepemkdiboiecpjlmdmighjkmfokbpm;https://clients2.google.com/service/update2/crx",
+  "mfhehppjhmmnlfbbopchdfldgimhfhfk;https://clients2.google.com/service/update2/crx",
+  "nhoiepgjfnecbnmhndhghcehecalhlnk;https://goguardian.com/licenses/update.php",
+  "pdldjapecechflnpdgpeiklnbigamggl;https://clients2.google.com/service/update2/crx",
+  "cgbbbjmgdpnifijconhamggjehlamcif;https://clients2.google.com/service/update2/crx"
+],
+  "ExtensionSettings": {
+  "epkahbohdeajpadlmelfkenmoeblkpem": {
+    "override_update_url": true
+  },
+  "haldlgldplgnggkjaafhelgiaglafanh": {
+    "blocked_permissions": []
+  },
+  "mfhehppjhmmnlfbbopchdfldgimhfhfk": {
+    "toolbar_pin": "force_pinned"
+  },
+  "nhoiepgjfnecbnmhndhghcehecalhlnk": {
+    "blocked_permissions": []
+  },
+  "niffbjhghfgccacnebkhbpmgbnhocapg": {
+    "blocked_permissions": []
+  }
+},
   "PasswordManagerEnabled": "true",
   "TaskManagerEndProcessEnabled": "true",
   "UptimeLimit": "null",
