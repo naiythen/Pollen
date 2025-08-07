77c7e915f757fb8462a0773baf8e6ab9159e9752
naiythen <98719576+naiythen@users.noreply.github.com>
2025-08-06 20:55:05 -0500

    Update RootFS.sh

--- RootFS.sh
+++ RootFS.sh
@@ -1,3 +1,3 @@
 sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk1 --remove_rootfs_verification --partitions 2
-sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk2 --remove_rootfs_verification --partitions 4
+sudo /usr/share/vboot/bin/make_dev_ssd.sh -i /dev/mmcblk1 --remove_rootfs_verification --partitions 4
 echo "RootFS has been disabled!"
